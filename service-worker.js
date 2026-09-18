083906dd596dc63b8ce97530a70205ecda5553a3
businesssolutionsao-hub <business.solutions.ao@gmail.com>
2026-09-18 13:26:52 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE = 'quittly-v091';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './ao-solutions-logo.png'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
